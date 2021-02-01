6b78851eb07cb146d8e889e9d3a907e17229d466
srntqn <serenityqn@gmail.com>
2021-02-01 14:10:14 +0300

    feat: create class Project and move version file logic to this class

---
+++ feeder/project.py
@@ -0,0 +1,22 @@
+import yaml
+from os import path
+
+
+class Project:
+    def __init__(self,
+                 project_name: str,
+                 version_file_name: str,
+                 version_file_path: str,
+                 git_url: str) -> None:
+        self.project_name: project_name
+        self.version_file_name = version_file_name
+        self.version_file_path = version_file_path
+        self.git_url = git_url
+
+    def get_project_name(self):
+        return self.get_project_name()
+
+    def get_app_version(self):
+        with open(path.join(self.version_file_path, self.version_file_name)) as file:
+            version_file = yaml.load(file, Loader=yaml.FullLoader)
+        return version_file['version']
