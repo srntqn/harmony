f37fcff0fe8386863666e3fb370f7931aa05955a
srntqn <serenityqn@gmail.com>
2021-02-23 16:26:58 +0300

    feat: check return value type for get_deployment call is V1Deployment

--- tests/test_get_deployment.py
+++ tests/test_get_deployment.py
@@ -1,11 +1,20 @@
 from harmony.k8s_cluster import KubernetesCluster
+from kubernetes.client import V1Deployment, V1Role
 from unittest import mock
+
+cluster = KubernetesCluster()
 
 
 def test_list_namespaced_deployment_call_during_get_deployment_call():
-    cluster = KubernetesCluster()
     cluster.apps.list_namespaced_deployment = mock.MagicMock()
     cluster.get_deployment('default', 'nginx')
     cluster.apps.list_namespaced_deployment.assert_called_once_with(watch=False,
                                                                     namespace='default',
                                                                     label_selector='app=nginx')
+
+
+def test_return_value_type_is_v1deployment():
+    deployment = cluster.get_deployment('default', 'nginx')
+    assert type(deployment) == V1Deployment
+
+
