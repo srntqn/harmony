2b297396fc53c9bcff1bc3f59df9d483f542aa44
srntqn <serenityqn@gmail.com>
2021-02-23 16:11:12 +0300

    feat: check that get_deployment call is calling a list_namespaced_deployment with the correct paramateres

--- tests/test_get_deployment.py
+++ tests/test_get_deployment.py
@@ -1,10 +1,11 @@
 from harmony.k8s_cluster import KubernetesCluster
+from unittest import mock
 
 
-def test_get_deployment(cluster: KubernetesCluster, ns: str, label: str):
-    deployment = cluster.get_deployment(ns, label)
-    assert deployment.metadata.labels['app'] == label
-    assert deployment.metadata.namespace == ns
-
-
-test_get_deployment(KubernetesCluster(), 'default', 'nginx')
+def test_list_namespaced_deployment_call_during_get_deployment_call():
+    cluster = KubernetesCluster()
+    cluster.apps.list_namespaced_deployment = mock.MagicMock()
+    cluster.get_deployment('default', 'nginx')
+    cluster.apps.list_namespaced_deployment.assert_called_once_with(watch=False,
+                                                                    namespace='default',
+                                                                    label_selector='app=nginx')
