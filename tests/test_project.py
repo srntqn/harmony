05e92222bd38246c9561fa9377cb58124452778f
srntqn <serenityqn@gmail.com>
2021-02-25 14:36:05 +0300

    feat: add tests for CoreConfig class

--- tests/test_project.py
+++ tests/test_project.py
@@ -3,23 +3,26 @@ from os import path
 
 import tempfile
 
-project = Project('test_project',
-                  'test_version_file.yaml',
-                  tempfile.gettempdir(),
+version_file_name = 'test_version_file.yaml'
+version_file_path = tempfile.gettempdir()
+project_name = 'test_project'
+
+project = Project(project_name,
+                  version_file_name,
+                  version_file_path,
                   'git@github.com:srntqn/sample_harmony_version_file_source.git')
 
 
 def test_get_project_name():
-    project_name = project.get_project_name()
-    assert project_name == 'test_project'
+    prj_name = project.get_project_name()
+    assert prj_name == project_name
 
 
 def test_get_app_version():
-    version_vile = path.join(tempfile.gettempdir(), "test_version_file.yaml")
+    version_file = path.join(version_file_path, version_file_name)
 
-    with open(version_vile, "w") as f:
+    with open(version_file, "w") as f:
         f.write("version: 1.0.0")
 
     app_version = project.get_app_version()
     assert app_version == "1.0.0"
-
