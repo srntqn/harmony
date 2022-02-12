6aebc64c1d317f21f3e2ded8d883cc45ed741c28
srntqn <serenityqn@gmail.com>
2022-02-12 20:01:20 +0100

    feat: create separate Worker class for reconcilation loop logic

--- harmony/cli.py
+++ harmony/cli.py
@@ -1,16 +1,11 @@
-import time
 import typer
 
 from . import __version__, __module_name__
 from .libs.k8s_cluster import KubernetesCluster
-from .libs.serializers.project import Project
 from .libs.serializers.config import Config
-from .libs.serializers.image import Image
-from .libs.k8s_deployment import K8sDeployment
-from .libs.worker import read_projects_from_config, fetch_app_version_from_vcs, sync_versions_in_vcs_and_cluster
+from .libs.worker import Worker
 
 CONFIG = Config()
-PROJECTS = read_projects_from_config(CONFIG.config_location)
 CLUSTER = KubernetesCluster(CONFIG.verify_ssl,
                             CONFIG.k8s_api_server,
                             CONFIG.k8s_api_key)
@@ -26,25 +21,5 @@ def version() -> str:
 
 @cli.command(help="Run Harmony")
 def run() -> None:
-    while True:
-        for prj_name, prj_spec in PROJECTS.items():
-
-            project = Project(**prj_spec)
-
-            deployment = K8sDeployment(CLUSTER.fetch_deployment(project.app_name, project.app_namespace))
-
-            version_in_vcs = fetch_app_version_from_vcs(project.vcs_url)
-
-            image = Image(**{
-                'name': deployment.print_image_name(),
-                'tag_in_cluster': deployment.print_image_tag(),
-                'tag_in_vcs': version_in_vcs
-            })
-
-            sync_versions_in_vcs_and_cluster(prj_name,
-                                             project,
-                                             deployment,
-                                             image,
-                                             CLUSTER)
-
-        time.sleep(20)
+    worker = Worker(CONFIG, CLUSTER)
+    worker.reconciliation_loop()
--- harmony/libs/worker.py
+++ harmony/libs/worker.py
@@ -3,6 +3,7 @@ import logging
 import requests
 import sys
 import yaml
+import time
 
 from .k8s_deployment import K8sDeployment
 from .k8s_cluster import KubernetesCluster
@@ -16,41 +17,74 @@ logging.basicConfig(stream=sys.stdout,
 logger = logging.getLogger(__name__)
 
 
-def read_projects_from_config(config_path: str):
+class Worker:
+    def __init__(self,
+                 config: Config,
+                 k8s_cluster: KubernetesCluster) -> None:
+        self.config = config
+        self.k8s_cluster = k8s_cluster
 
-    with open(config_path) as file:
-        config = yaml.load(file, Loader=yaml.FullLoader)
+    def _read_projects_from_config(config_path: str):
 
-    logger.info(f"Reading the following projects data: {config}")
+        with open(config_path) as file:
+            config = yaml.load(file, Loader=yaml.FullLoader)
 
-    return config
+        logger.info(f"Reading the following projects data: {config}")
 
-
-def fetch_app_version_from_vcs(vcs_url: str) -> str:
-
-    r = yaml.load(requests.get(vcs_url).content, Loader=yaml.FullLoader)
-
-    logger.debug(f"The content of {vcs_url} is {r}")
-
-    return r['version']
+        return config
 
 
-def sync_versions_in_vcs_and_cluster(project_name: str,
-                                     project: Project,
-                                     k8s_deployment: K8sDeployment,
-                                     image: Image,
-                                     k8s_cluster: KubernetesCluster):
+    def _fetch_app_version_from_vcs(vcs_url: str) -> str:
 
-    if image.tag_in_vcs != image.tag_in_cluster:
-        k8s_cluster.patch_deployment(k8s_deployment.output_k8s_deployment_object(),
-                                     f'{image.name}:{image.tag_in_vcs}',
-                                     project.app_name,
-                                     project.app_namespace)
+        r = yaml.load(requests.get(vcs_url).content, Loader=yaml.FullLoader)
 
-        logger.info(f"""
-                Image version for {project_name} in cluster: {image.tag_in_cluster}.
-                Image version for {project_name} in git: {image.tag_in_vcs}.
-                Updating deployment...
-                """)
-    else:
-        logger.info(f"Nothing was changed for {project_name}. Versions are equal.")
+        logger.debug(f"The content of {vcs_url} is {r}")
+
+        return r['version']
+
+
+    def _sync_versions_in_vcs_and_cluster(project_name: str,
+                                         project: Project,
+                                         k8s_deployment: K8sDeployment,
+                                         image: Image,
+                                         k8s_cluster: KubernetesCluster):
+
+        if image.tag_in_vcs != image.tag_in_cluster:
+            k8s_cluster.patch_deployment(k8s_deployment.output_k8s_deployment_object(),
+                                         f'{image.name}:{image.tag_in_vcs}',
+                                         project.app_name,
+                                         project.app_namespace)
+
+            logger.info(f"""
+                    Image version for {project_name} in cluster: {image.tag_in_cluster}.
+                    Image version for {project_name} in git: {image.tag_in_vcs}.
+                    Updating deployment...
+                    """)
+        else:
+            logger.info(f"Nothing was changed for {project_name}. Versions are equal.")
+
+
+    def reconciliation_loop(self) -> None:
+        while True:
+            for prj_name, prj_spec in self._read_projects_from_config(self.config.config_location).items():
+
+                project = Project(**prj_spec)
+
+                deployment = K8sDeployment(self.k8s_cluster.fetch_deployment(project.app_name, project.app_namespace))
+
+                version_in_vcs = self._fetch_app_version_from_vcs(project.vcs_url)
+
+                image = Image(**{
+                    'name': deployment.print_image_name(),
+                    'tag_in_cluster': deployment.print_image_tag(),
+                    'tag_in_vcs': version_in_vcs
+                })
+
+                self._sync_versions_in_vcs_and_cluster(prj_name,
+                                                       project,
+                                                       deployment,
+                                                       image,
+                                                       self.k8s_cluster)
+
+            time.sleep(20)
+
